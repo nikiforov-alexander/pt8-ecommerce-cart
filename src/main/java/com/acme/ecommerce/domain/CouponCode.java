21a336b03b58884fe8564a4355ca83151776c58f
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-07 21:02:15 +0200

    added `@Size` validation constraint to set limit on `code` length

--- src/main/java/com/acme/ecommerce/domain/CouponCode.java
+++ src/main/java/com/acme/ecommerce/domain/CouponCode.java
@@ -3,10 +3,13 @@ package com.acme.ecommerce.domain;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.Size;
+
 @Component
 @Scope("session")
 public class CouponCode {
 
+    @Size(min = 5, max = 10, message = "Coupon code should be between 5 and 10 characters")
 	private String code;
 	
 	public String getCode() {
