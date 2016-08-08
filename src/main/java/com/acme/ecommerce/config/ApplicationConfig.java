c30addcdd62a612f84a22747ed5d8eb2236f98d4
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-08 11:18:48 +0200

    commented properties file declaration, so that app works for now

--- src/main/java/com/acme/ecommerce/config/ApplicationConfig.java
+++ src/main/java/com/acme/ecommerce/config/ApplicationConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("file:${properties.home}/eCommerce.properties")
+//@PropertySource("file:${properties.home}/eCommerce.properties")
 public class ApplicationConfig {
 
 	//To resolve ${} in @Value
