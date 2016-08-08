1f681bfb9aaf5de2eb637973e2dbb07462bf02ac
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-08 20:23:10 +0200

    added very important "@NotEmpty" annotation, this ensures my Coupon is not null and makes testing wrong code available

--- src/main/java/com/acme/ecommerce/domain/CouponCode.java
+++ src/main/java/com/acme/ecommerce/domain/CouponCode.java
@@ -1,5 +1,6 @@
 package com.acme.ecommerce.domain;
 
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import javax.validation.constraints.Size;
 @Scope("session")
 public class CouponCode {
 
+	@NotEmpty(message = "Coupon code cannot be empty")
     @Size(min = 5, max = 10, message = "Coupon code should be between 5 and 10 characters")
 	private String code;
 	
